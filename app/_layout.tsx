a238f53218ae846b0f24ca60077cd92149713582
Rafael Morais <rafaelcanical@gmail.com>
2024-02-27 23:14:22 +0100

    Added new splash screen & prepared for loading time

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,21 @@
-import { Stack } from 'expo-router'
+import { SplashScreen, Stack } from 'expo-router'
+import { useEffect } from 'react'
 import ThemeProvider from '~providers/ThemeProvider'
 
 if (__DEV__) {
   import('../reactotron').then(() => console.log('Reactotron Configured'))
 }
 
+SplashScreen.preventAutoHideAsync()
+
 export default function Layout() {
+  useEffect(() => {
+    // Placeholder timeout before db search is developer
+    setTimeout(() => {
+      SplashScreen.hideAsync()
+    }, 2000)
+  }, [])
+
   return (
     <ThemeProvider>
       <Stack screenOptions={{ headerShown: false }}>

796b214d65345eafa98157e187749b68dc0e5a29
Rafael Morais <rafaelcanical@gmail.com>
2024-02-29 23:39:06 +0100

    Added disk and keyboard-down icons

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -30,6 +30,26 @@ const Icon = ({ type, color, size }: TIconProps) => {
         />
       </Svg>
     )
+  } else if (type === 'keyboard-down') {
+    const defaultWidth = size ? size : 20
+    return (
+      <Svg width={defaultWidth} height={defaultWidth} viewBox="0 0 20 20" fill="none">
+        <Path
+          fill={finalColor}
+          d="M15.833 1.667H9.167c.013 0-.221 0 0 0h-5A4.172 4.172 0 0 0 0 5.833v3.334a4.172 4.172 0 0 0 4.167 4.166h11.666A4.172 4.172 0 0 0 20 9.167V5.833a4.172 4.172 0 0 0-4.167-4.166Zm2.5 7.5a2.5 2.5 0 0 1-2.5 2.5H4.167a2.5 2.5 0 0 1-2.5-2.5V5.833a2.5 2.5 0 0 1 2.5-2.5h11.666a2.5 2.5 0 0 1 2.5 2.5v3.334Z"
+        />
+        <Path
+          fill={finalColor}
+          d="M12.5 8.333h-5A.833.833 0 0 0 7.5 10h5a.833.833 0 0 0 0-1.667ZM8.333 6.667h.834a.833.833 0 1 0 0-1.667h-.834a.833.833 0 0 0 0 1.667ZM15.833 5H12.5a.833.833 0 1 0 0 1.667h3.333a.833.833 0 0 0 0-1.667ZM5 5h-.833a.833.833 0 1 0 0 1.667H5A.833.833 0 1 0 5 5ZM4.167 10a.833.833 0 1 0 0-1.667.833.833 0 0 0 0 1.667ZM15.833 10a.833.833 0 1 0 0-1.667.833.833 0 0 0 0 1.667Z"
+        />
+        <Path
+          fill={finalColor}
+          fillRule="evenodd"
+          d="M6.077 15.244a.833.833 0 0 1 1.179 0L10 17.988l2.744-2.744a.833.833 0 1 1 1.179 1.179l-3.334 3.333a.833.833 0 0 1-1.178 0l-3.334-3.333a.833.833 0 0 1 0-1.179Z"
+          clipRule="evenodd"
+        />
+      </Svg>
+    )
   } else if (type === 'settings') {
     const defaultWidth = size ? size : 24
     return (
@@ -53,9 +73,23 @@ const Icon = ({ type, color, size }: TIconProps) => {
         />
       </Svg>
     )
+  } else if (type === 'disk') {
+    const defaultWidth = size ? size : 20
+    return (
+      <Svg width={defaultWidth} height={defaultWidth} viewBox="0 0 20 20" fill="none">
+        <Path
+          fill={finalColor}
+          d="M10 8.333A3.333 3.333 0 1 0 10 15a3.333 3.333 0 0 0 0-6.667Zm0 5A1.667 1.667 0 1 1 10 10a1.667 1.667 0 0 1 0 3.334Z"
+        />
+        <Path
+          fill={finalColor}
+          d="M18.78 3.435 16.565 1.22A4.138 4.138 0 0 0 13.619 0H4.167A4.172 4.172 0 0 0 0 4.167v11.666A4.172 4.172 0 0 0 4.167 20h11.666A4.172 4.172 0 0 0 20 15.833V6.381a4.14 4.14 0 0 0-1.22-2.946Zm-4.613-1.702V2.5a2.5 2.5 0 0 1-2.5 2.5H8.333a2.5 2.5 0 0 1-2.5-2.5v-.833h7.786c.185.001.368.024.548.066Zm4.166 14.1a2.5 2.5 0 0 1-2.5 2.5H4.167a2.5 2.5 0 0 1-2.5-2.5V4.167a2.5 2.5 0 0 1 2.5-2.5V2.5a4.172 4.172 0 0 0 4.166 4.167h3.334a4.16 4.16 0 0 0 4.135-3.854l1.8 1.8c.467.47.73 1.105.731 1.768v9.452Z"
+        />
+      </Svg>
+    )
   }
 }
 
-export type TIcons = 'box' | 'plus' | 'settings' | 'keyboard-down' | 'trash' | 'haptics' | 'sounds' | 'check'
+export type TIcons = 'box' | 'plus' | 'settings' | 'keyboard-down' | 'trash' | 'haptics' | 'sounds' | 'check' | 'disk'
 
 export default Icon
